7b26f4639f230844e3cd1492c23b0db216149c18
shreyabhardwaj2394 <shreya2394bhardwaj@gmail.com>
2017-07-10 13:14:40 +0530

    Q1

---
+++ src/main/java/com/exercise/StudentController.java
@@ -0,0 +1,28 @@
+package com.exercise;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Shreya on 7/9/2017.
+ */
+
+public class StudentController extends AbstractController
+ {
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView model=new ModelAndView("index");
+        model.addObject("message","HELLO FROM INDEX.JSP");
+        return model;
+    }
+
+
+}
